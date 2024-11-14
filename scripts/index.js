90dd47f944fc04bb7ef9179b70f6e7e9e07f5c4e
Kerrodar <Kirill555dg@gmail.com>
2024-11-14 21:51:34 +0300

    feat: add profile form submit

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,10 @@
 import {initialCards} from './cards.js';
 
-// @todo: Темплейт карточки
+// Темплейт карточки
 
 const cardTemplate = document.querySelector('#card-template').content;
 
-
-
-
-
-// @todo: DOM узлы
+// DOM узлы
 
 const placesList = document.querySelector('.places__list');
 
@@ -23,7 +19,7 @@ const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
 
 
-// @todo: Функция создания карточки
+// Функция создания карточки
 function createCard(src, title){
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
     const cardImage = cardElement.querySelector('.card__image');
@@ -47,13 +43,37 @@ function createCard(src, title){
     return cardElement;
 }
 
-// @todo: Функция удаления карточки
 
 
-
-// @todo: Вывести карточки на страницу
+// Вывести карточки на страницу
 
 initialCards.forEach(cardInfo => placesList.append(createCard(cardInfo['link'], cardInfo['name'])));
+
+
+
+// Находим форму в DOM
+const profileFormElement = document.querySelector('.popup__form');
+// Находим поля формы в DOM
+const nameInput = profileFormElement.querySelector('.popup__input_type_name');
+const jobInput = profileFormElement.querySelector('.popup__input_type_description');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+    profileTitle.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
+
+    closeModal(profilePopup);
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+
+
+// Открытие и закрытие поп-апа
 
 function openModal(popup) {      
     popup.classList.add('popup_is-opened');
@@ -67,10 +87,12 @@ function closeModal(popup) {
 
 profileEditButton.addEventListener('click', event => {
 
-    profilePopup.querySelector('.popup__input_type_name').value = profileTitle.textContent;
-    profilePopup.querySelector('.popup__input_type_description').value = profileDescription.textContent;
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileDescription.textContent;
 
     openModal(profilePopup);
 });
 
 addCardButton.addEventListener('click', event => openModal(cardPopup));
+
+
