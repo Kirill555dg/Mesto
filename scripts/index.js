a56b64bdcbb5deaab9456d108a718e1f58e93ae2
Kerrodar <Kirill555dg@gmail.com>
2024-11-14 22:29:12 +0300

    feat: add card form and card popup

--- scripts/index.js
+++ scripts/index.js
@@ -8,8 +8,8 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 const placesList = document.querySelector('.places__list');
 
-const profilePopup = document.querySelector('.popup_type_edit');
-const cardPopup = document.querySelector('.popup_type_new-card');
+const profileFormPopup = document.querySelector('.popup_type_edit');
+const cardFormPopup = document.querySelector('.popup_type_new-card');
 const imagePopup = document.querySelector('.popup_type_image');
 
 const profileEditButton = document.querySelector('.profile__edit-button');
@@ -20,15 +20,20 @@ const profileDescription = document.querySelector('.profile__description');
 
 
 // Функция создания карточки
-function createCard(src, title){
+function createCard(link, title){
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
     const cardImage = cardElement.querySelector('.card__image');
     const cardTitle = cardElement.querySelector('.card__title');
     const cardLikeButton = cardElement.querySelector('.card__like-button');
     const cardDeleteButton = cardElement.querySelector('.card__delete-button');
 
-    cardImage.setAttribute('src', src);
+    cardImage.setAttribute('src', link);
     cardImage.setAttribute('alt', title);
+    cardImage.addEventListener('click', event => {
+        imagePopup.querySelector('.popup__image').src = link;
+        imagePopup.querySelector('.popup__caption').textContent = title;
+        openModal(imagePopup);
+    });
 
     cardTitle.textContent = title;
 
@@ -39,6 +44,8 @@ function createCard(src, title){
     cardDeleteButton.addEventListener('click', event =>
         event.target.closest('.places__item').remove()
     );
+
+    
 
     return cardElement;
 }
@@ -52,7 +59,7 @@ initialCards.forEach(cardInfo => placesList.append(createCard(cardInfo['link'], 
 
 
 // Находим форму в DOM
-const profileFormElement = document.querySelector('.popup__form');
+const profileFormElement = profileFormPopup.querySelector('.popup__form');
 // Находим поля формы в DOM
 const nameInput = profileFormElement.querySelector('.popup__input_type_name');
 const jobInput = profileFormElement.querySelector('.popup__input_type_description');
@@ -65,12 +72,33 @@ function handleProfileFormSubmit(evt) {
     profileTitle.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
 
-    closeModal(profilePopup);
+    closeModal(profileFormPopup);
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+
+
+// Находим форму в DOM
+const cardFormElement = cardFormPopup.querySelector('.popup__form');
+// Находим поля формы в DOM
+const titleInput = cardFormElement.querySelector('.popup__input_type_card-name');
+const linkInput = cardFormElement.querySelector('.popup__input_type_url');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleCardFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+    placesList.prepend(createCard(linkInput.value, titleInput.value));
+
+    closeModal(cardFormPopup);
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+cardFormElement.addEventListener('submit', handleCardFormSubmit);
 
 
 // Открытие и закрытие поп-апа
@@ -80,7 +108,7 @@ function openModal(popup) {
     popup.querySelector('.popup__close').addEventListener('click', event => closeModal(popup));
 } 
 
-function closeModal(popup) {      
+function closeModal(popup) {
     popup.classList.remove('popup_is-opened');
 }
 
@@ -90,9 +118,9 @@ profileEditButton.addEventListener('click', event => {
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileDescription.textContent;
 
-    openModal(profilePopup);
+    openModal(profileFormPopup);
 });
 
-addCardButton.addEventListener('click', event => openModal(cardPopup));
+addCardButton.addEventListener('click', event => openModal(cardFormPopup));
 
 
