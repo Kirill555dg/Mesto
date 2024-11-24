ad0c66d564fb0bb590d96b5a30ffb932c6f208ca
Kerrodar <Kirill555dg@gmail.com>
2024-11-24 14:22:56 +0300

    feat: add closing a pop-up with the escape key

--- scripts/index.js
+++ scripts/index.js
@@ -61,8 +61,15 @@ function createCard(link, title) {
   return cardElement
 }
 
-function closeOnOverlay(evt) {
+function closeModalOnOverlay(evt) {
   closeModal(evt.target)
+}
+
+function closeModalByEsc(evt) {
+  if (evt.key === "Escape") {
+    const openedModal = document.querySelector('.popup_is-opened')
+    closeModal(openedModal)
+  }
 }
 
 // Функция открытия поп-апа
@@ -71,18 +78,19 @@ function openModal(popup) {
 
   popup.addEventListener('mouseover', event => {
     if (event.target === event.currentTarget) {
-      popup.addEventListener('click', closeOnOverlay)
+      popup.addEventListener('click', closeModalOnOverlay)
       event.target.style.cursor = "pointer";
     }
   })
 
   popup.addEventListener('mouseout', event => {
     if (event.target === event.currentTarget) {
-      popup.removeEventListener('click', closeOnOverlay)
+      popup.removeEventListener('click', closeModalOnOverlay)
       event.target.style.cursor = "default";
     }
   })
 
+  document.addEventListener('keydown', closeModalByEsc)
   popup.querySelector('.popup__close').addEventListener('click', () => {
     closeModal(popup)
   })
@@ -91,6 +99,7 @@ function openModal(popup) {
 // Функция закрытия поп-апа
 function closeModal(popup) {
   popup.classList.remove('popup_is-opened')
+  document.removeEventListener('keydown', closeModalByEsc)
 }
 
 
